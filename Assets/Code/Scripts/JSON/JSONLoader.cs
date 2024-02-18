965365a8656c24994460da521aeb270e8ba9c3d3
Tojikimado <76056480+Tojikimado@users.noreply.github.com>
2024-02-18 17:17:58 +0100

    Fix UTF8

--- Assets/Code/Scripts/JSON/JSONLoader.cs
+++ Assets/Code/Scripts/JSON/JSONLoader.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.IO;
+using System.Text;
 using Newtonsoft.Json;
 
 public class JSONLoader : MonoBehaviour
@@ -17,7 +18,7 @@ public class JSONLoader : MonoBehaviour
     public SentencesTemplates LoadSentencesTemplates(string fileName)
     {
         string jsonFilePath = Path.Combine(Application.dataPath, "Resources", fileName + ".json");
-        string jsonString = File.ReadAllText(jsonFilePath);
+        string jsonString = File.ReadAllText(jsonFilePath, Encoding.UTF8);
         SentencesTemplates sentencesTemplates = JsonConvert.DeserializeObject<SentencesTemplates>(jsonString);
 
         return sentencesTemplates;
